d9e710b8d23d2811c9f3400afc96b65597bcb00e
Joseph Rissler <joseph.rissler@gmail.com>
2023-08-20 19:11:14 -0500

    Left out config.py from last commit

--- src/config.py
+++ src/config.py
@@ -1,21 +1,24 @@
 import sys
 import discord
+from types import SimpleNamespace
 
-testing = (len(sys.argv) > 1 and sys.argv[1] == 'test')
+config = SimpleNamespace()
 
-gamemaster = discord.Object(id=373840269050642442) #undecim11
+def init_config(mode=None):
+    
+    config.gamemaster = discord.Object(id=373840269050642442) #undecim11
 
-if testing:
-    tokenfile = "TOKEN.testing"
-    datafile = "testing.pickle"
-    channel = discord.Object(id=1141553139779125258)
-    guild = discord.Object(id=436960665647972368)
-    point_emoji = "👁️"
-else:
-    tokenfile = "TOKEN"
-    datafile = "data.pickle"
-    channel = discord.Object(id=1135339346443124817)
-    guild = discord.Object(id=1135339345990135880)
-    point_emoji = discord.PartialEmoji(name='point', id=1137681095614275646)
+    if mode == 'test':
+        config.tokenfile = "TOKEN.testing"
+        config.datafile = "testing.pickle"
+        config.channel = discord.Object(id=1141553139779125258)
+        config.guild = discord.Object(id=436960665647972368)
+        config.point_emoji = "👁️"
+    else:
+        config.tokenfile = "TOKEN"
+        config.datafile = "data.pickle"
+        config.channel = discord.Object(id=1135339346443124817)
+        config.guild = discord.Object(id=1135339345990135880)
+        config.point_emoji = discord.PartialEmoji(name='point', id=1137681095614275646)
 
-with open(tokenfile) as f: token = f.read().strip()
+    with open(config.tokenfile) as f: config.token = f.read().strip()
